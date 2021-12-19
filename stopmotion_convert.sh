a1255e026ed0d4933aa1cd9d414a76a5f05d8592
garbaz <garbaz@t-online.de>
2021-12-19 11:12:50 +0100

    stopmotion

---
+++ stopmotion_convert.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FRAMERATE=10
+
+TMPFOLDER=".convert_tmp"
+FORMATTINGSTRING="%06d"
+
+mkdir "$TMPFOLDER"
+cd "$TMPFOLDER" || exit
+i=0
+ext=${1##*.}
+for f in "$@";do
+    ipadded=$(printf "$FORMATTINGSTRING" $i)
+    ln -s "../$f" "$ipadded.$ext"
+    i=$((i+1))
+done
+cd ..
+ffmpeg -r "$FRAMERATE" -i "$TMPFOLDER/$FORMATTINGSTRING.$ext" -vcodec libx264 out.mp4
+rm -r "$TMPFOLDER"
